4e8cd914dad02819d9af9f020a64309706e83ac8
pcheloshock <kraniys@yandex.ru>
2026-03-02 21:54:18 +0500

    Создание непонятной конструкции 0.0.1

---
+++ src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -0,0 +1,47 @@
+package ru.practicum.shareit.booking;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.booking.dto.BookingDto;
+import ru.practicum.shareit.booking.dto.BookingState;
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping(path = "/bookings")
+@RequiredArgsConstructor
+public class BookingController {
+    private final BookingService bookingService;
+
+    @PostMapping
+    public BookingDto createBooking(@RequestHeader("X-Sharer-User-Id") Long userId,
+                                    @RequestBody BookingDto bookingDto) {
+        return bookingService.createBooking(userId, bookingDto);
+    }
+
+    @PatchMapping("/{bookingId}")
+    public BookingDto approveBooking(@RequestHeader("X-Sharer-User-Id") Long userId,
+                                     @PathVariable Long bookingId,
+                                     @RequestParam Boolean approved) {
+        return bookingService.approveBooking(userId, bookingId, approved);
+    }
+
+    @GetMapping("/{bookingId}")
+    public BookingDto getBookingById(@RequestHeader("X-Sharer-User-Id") Long userId,
+                                     @PathVariable Long bookingId) {
+        return bookingService.getBookingById(userId, bookingId);
+    }
+
+    @GetMapping
+    public List<BookingDto> getUserBookings(@RequestHeader("X-Sharer-User-Id") Long userId,
+                                            @RequestParam(defaultValue = "ALL") BookingState state) {
+        return bookingService.getUserBookings(userId, state);
+    }
+
+    @GetMapping("/owner")
+    public List<BookingDto> getOwnerBookings(@RequestHeader("X-Sharer-User-Id") Long userId,
+                                             @RequestParam(defaultValue = "ALL") BookingState state) {
+        return bookingService.getOwnerBookings(userId, state);
+    }
+}
