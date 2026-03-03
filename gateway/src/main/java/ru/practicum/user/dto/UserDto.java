7c05a7eae429446295c8493cb588576444f5760a
pcheloshock <kraniys@yandex.ru>
2026-03-04 00:52:34 +0500

    Создание непонятной конструкции 0.0.1

--- gateway/src/main/java/ru/practicum/user/dto/UserDto.java
+++ gateway/src/main/java/ru/practicum/user/dto/UserDto.java
@@ -13,10 +13,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserDto {
     private Long id;
-    
+
     @NotBlank(message = "Имя не может быть пустым")
     private String name;
-    
+
     @NotBlank(message = "Email не может быть пустым")
     @Email(message = "Некорректный формат email")
     private String email;
