6bca4cfd17a1aaa38ade59339f4d7350b3bbeb88
pcheloshock <kraniys@yandex.ru>
2026-03-05 20:57:41 +0500

    Fix trailing whitespace in HealthController

--- server/src/main/java/ru/practicum/shareit/health/HealthController.java
+++ server/src/main/java/ru/practicum/shareit/health/HealthController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
-    
     @GetMapping("/actuator/health")
     public String health() {
         return "{\"status\":\"UP\"}";
