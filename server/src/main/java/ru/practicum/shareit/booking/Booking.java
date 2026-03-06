29c5588e7fea4a1a7ff94348e984df070442bbbc
pcheloshock <kraniys@yandex.ru>
2026-03-06 18:48:06 +0500

    Создание непонятной конструкции 0.0.6

--- server/src/main/java/ru/practicum/shareit/booking/Booking.java
+++ server/src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import ru.practicum.shareit.booking.dto.BookingStatus;
 import ru.practicum.shareit.item.Item;
 import ru.practicum.shareit.user.User;
 import java.time.LocalDateTime;
